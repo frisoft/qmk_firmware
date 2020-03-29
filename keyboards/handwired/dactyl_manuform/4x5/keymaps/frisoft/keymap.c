bd2bda929805f7b8649a69242ecb40eb51da6c7d
Andrea Frigido <andrea@frisoft.it>
2020-03-29 14:45:24 +0100

    numbers on the home row

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/frisoft/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/frisoft/keymap.c
@@ -180,8 +180,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */ 
 
 [_RAISE3] = LAYOUT( \
-  KC_1,         KC_2,          KC_3,         KC_4,         KC_5,                             KC_6,    KC_7,         KC_8,         KC_9,          KC_0,          \
   KC_EXLM,      KC_AT,         KC_HASH,      KC_DLR,       KC_PERC,                          KC_CIRC, KC_AMPR,      KC_ASTR,      KC_LPRN,       KC_RPRN,       \
+  KC_1,         KC_2,          KC_3,         KC_4,         KC_5,                             KC_6,    KC_7,         KC_8,         KC_9,          KC_0,          \
   SFT_T(KC_F1), LGUI_T(KC_F2), ALT_T(KC_F3), CTL_T(KC_F4), KC_F5,                            KC_F6,   CTL_T(KC_F7), ALT_T(KC_F8), LGUI_T(KC_F9), SFT_T(KC_F10), \
                 KC_F11,        KC_F12,                                                                              ____,         ____,                         \
                                                                     ____, ____,  ____, ____,                                                                    \
